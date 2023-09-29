05217de3d2fec06e8ab9028c348d62ac96fcdf95
MaghodiyaVivek <123167310+MaghodiyaVivek@users.noreply.github.com>
2023-09-29 15:05:56 +0530

    Add files via upload

---
+++ Assignment/Module-1/HelloEx.java
@@ -0,0 +1,10 @@
+package javaprogram;
+
+public class HelloEx {
+
+	public static void main(String[] args) {
+		System.out.print("hello wold");
+
+	}
+
+}
---
+++ Assignment/Module-1/Program10.java
@@ -0,0 +1,27 @@
+package javaprogram;
+
+public class Program10 
+{
+
+	//check the given number is odd or even.
+	
+	public static void main(String[] args) 
+	{
+		
+		
+		int num = 5;
+		int v = num%2;
+		if(v==0)
+		{
+			System.out.println("Number is even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+	
+	
+	}
+
+
+}
